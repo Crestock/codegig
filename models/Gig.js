9c66b88261b9d81a3110c878a4274e1fc14b0da1
Crestock <47856835+Crestock@users.noreply.github.com>
2019-10-22 23:06:48 +0100

    Got app working as intended

--- models/Gig.js
+++ models/Gig.js
@@ -1,22 +1,25 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
+const Sequelize = require("sequelize");
+const db = require("../config/database");
 
-const Gig = db.define('gig', {
-  title: {
-    type: Sequelize.STRING
+const Gig = db.define(
+  "gig",
+  {
+    title: Sequelize.STRING
   },
-  technologies: {
-    type: Sequelize.STRING
+  {
+    technologies: Sequelize.STRING
   },
-  description: {
-    type: Sequelize.STRING
+
+  {
+    description: Sequelize.STRING
   },
-  budget: {
-    type: Sequelize.STRING
+  {
+    budget: Sequelize.STRING
   },
-  contact_email: {
-    type: Sequelize.STRING
-  }
-})
+  {
+    contact_email: Sequelize.STRING
+  },
+  { timestamps: false }
+);
 
 module.exports = Gig;
