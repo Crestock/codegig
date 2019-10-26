4b74a44ace1875b4bcdfd596d35378d396a42f28
Crestock <47856835+Crestock@users.noreply.github.com>
2019-10-26 14:36:56 +0100

    Started working on product form routing

---
+++ models/Product.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database");
+
+const Product = db.define(
+  "product",
+  {
+    Name: Sequelize.STRING
+  },
+  {
+    MinL: Sequelize.STRING
+  },
+  {
+    MinW: Sequelize.STRING
+  },
+
+  {
+    MinT: Sequelize.STRING
+  },
+  
+  { timestamps: false }
+);
+
+module.exports = Gig;
