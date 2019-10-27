e0309246fcca61073b0be101b7bdd8e88b316bb5
Crestock <47856835+Crestock@users.noreply.github.com>
2019-10-27 14:57:12 +0000

    addded product views (pre routing and testing)

--- routes/products.js
+++ routes/products.js
@@ -1,82 +1,80 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../config/database');
-const Product = require('../models/Product');
-const Sequelize = require('sequelize');
+const db = require("../config/database");
+const Product = require("../models/Product");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-// Get gig list
-router.get('/', (req, res) => 
-  Gig.findAll()
-    .then(gigs => res.render('gigs', {
-        gigs
-      }))
-    .catch(err => console.log(err)));
+// Get product list
+router.get("/", (req, res) =>
+  Product.findAll()
+    .then(products =>
+      res.render("products", {
+        products
+      })
+    )
+    .catch(err => console.log(err))
+);
 
-// Display add gig form
-router.get('/add', (req, res) => res.render('add'));
+// Display add product form
+router.get("/add", (req, res) => res.render("add"));
 
-// Add a gig
-router.post('/add', (req, res) => {
-  let { title, technologies, budget, description, contact_email } = req.body;
+// Add a product
+router.post("/add", (req, res) => {
+  let { name, minL, minW, minT } = req.body;
   let errors = [];
 
   // Validate Fields
-  if(!title) {
-    errors.push({ text: 'Please add a title' });
+  if (!name) {
+    errors.push({ text: "Please add a product name" });
   }
-  if(!technologies) {
-    errors.push({ text: 'Please add some technologies' });
+  if (!minL) {
+    errors.push({ text: "Please add a minimum Length" });
   }
-  if(!description) {
-    errors.push({ text: 'Please add a description' });
+  if (!minW) {
+    errors.push({ text: "Please add a minimum Width" });
   }
-  if(!contact_email) {
-    errors.push({ text: 'Please add a contact email' });
+  if (!minT) {
+    errors.push({ text: "Please add a minimum Thickness" });
   }
 
   // Check for errors
-  if(errors.length > 0) {
-    res.render('add', {
+  if (errors.length > 0) {
+    res.render("add", {
       errors,
-      title, 
-      technologies, 
-      budget, 
-      description, 
-      contact_email
+      name,
+      minL,
+      minW,
+      minT
     });
   } else {
-    if(!budget) {
-      budget = 'Unknown';
-    } else {
-      budget = `$${budget}`;
-    }
+    if (!name) {
+      name = "Product1";
+    } 
 
-    // Make lowercase and remove space after comma
-    technologies = technologies.toLowerCase().replace(/, /g, ',');
+    
 
     // Insert into table
-    Gig.create({
-      title,
-      technologies,
-      description,
-      budget,
-      contact_email
+    Product.create({
+      name,
+      minL,
+      minW,
+      minT
     })
-      .then(gig => res.redirect('/gigs'))
+      .then(product => res.redirect("/products"))
       .catch(err => console.log(err));
   }
 });
 
-// Search for gigs
-router.get('/search', (req, res) => {
+// Search for products
+router.get("/search", (req, res) => {
   let { term } = req.query;
 
   // Make lowercase
   term = term.toLowerCase();
 
-  Gig.findAll({ where: { technologies: { [Op.like]: '%' + term + '%' } } })
-    .then(gigs => res.render('gigs', { gigs }))
+  Product.findAll({ where: { name: { [Op.like]: "%" + term + "%" } } })
+    .then(products => res.render("products", { products }))
     .catch(err => console.log(err));
 });
 
